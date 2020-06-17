6806654258a9023ab64cde77f10e12004a4f5fef
DariusPirvulescu <organicdarius@gmail.com>
2020-06-17 16:10:20 +0300

    added index, show, new, create actions to restaurant controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
 # Restaurant
@@ -23,10 +16,5 @@ puts 'creating restaurants'
   restaur = Restaurant.create!({ name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone_in_e164, category: category.sample })
   puts "creating restaurant #{restaur.name}"
 end
-
-# puts "creating reviews"
-# 20.times do
-#   Review.create(content: Faker::Lorem.sentence(word_count: 6), rating: rand(0..5), restaurant_id)
-# end
 
 puts 'finished'
