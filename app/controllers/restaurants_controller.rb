aa4bcefdc6397a8da4320344a54dc934171b6b90
DariusPirvulescu <organicdarius@gmail.com>
2020-06-18 13:11:20 +0300

    modify the redirect for restaurant(create) and review(new) to pass the rake

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,7 +14,7 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.create(restaurant_params)
     if @restaurant.save
-      redirect_to restaurants_path
+      redirect_to @restaurant
     else
       render :new
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,18 @@ category = ['chinese', 'italian', 'japanese', 'french', 'belgian']
 # rating = rand(0..5)
 # restaurant_id = rand(0..10)
 
-puts 'creating restaurants'
-10.times do
-  restaur = Restaurant.create!({ name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone_in_e164, category: category.sample })
-  puts "creating restaurant #{restaur.name}"
+# puts 'creating restaurants'
+# 10.times do
+#   restaur = Restaurant.create!({ name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone_in_e164, category: category.sample })
+#   puts "creating restaurant #{restaur.name}"
+# end
+
+puts "creating reviews"
+20.times do
+  rev = Review.new({content: Faker::Lorem.sentence(word_count: 7), rating: rand(0..5), restaurant_id: rand(1..12)})
+  rest = Restaurant.find(rev.restaurant_id)
+  puts "reviewing #{rest.name}"
+  rev.save
 end
 
 puts 'finished'
