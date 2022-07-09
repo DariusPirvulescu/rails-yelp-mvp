2c31548840e6c9a4d4d4f35ffbf6c9dfec25fd46
Darius Pirvulescu <organicdarius@gmail.com>
2022-07-09 19:13:52 +0100

    Modify routes to allow reviews edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => "pages#home"
   resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, shallow: true
   end
 end
