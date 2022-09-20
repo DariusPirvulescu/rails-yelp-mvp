dc1b465a7eb37e764b5ba5fef30d48582826ea60
Darius Pirvulescu <organicdarius@gmail.com>
2022-09-20 17:13:41 +0100

    Format restaurant.rb

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,17 +1,17 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews, :dependent => :destroy
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
-  validates :category, :inclusion => { :in => ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
 
   # Used by to_json to only return the some chosen attributes
-  # https://api.rubyonrails.org/classes/ActiveModel/Serializers/JSON.html#method-i-as_json 
+  # https://api.rubyonrails.org/classes/ActiveModel/Serializers/JSON.html#method-i-as_json
   def as_json(options={})
-    { 
-      :name => self.name,
-      :phone_number => self.phone_number,
-      :category => self.category
+    {
+      name: self.name,
+      phone_number: self.phone_number,
+      category: self.category
     }
     # super(:only => [:name])
   end
